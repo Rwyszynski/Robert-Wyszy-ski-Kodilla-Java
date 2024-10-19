1b658ba50a68855d0be3c7e76756cf0040425293
Robert Wyszynski <robertointerwento@gmail.com>
2024-10-19 21:11:13 +0200

    6.4 Pisanie testów za pomoc JUnit poprawka

--- kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -1,7 +1,10 @@
 package com.kodilla.testing.weather.stub;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Collections;
 
 public class WeatherForecast {
     private Temperatures temperatures;
@@ -21,5 +24,33 @@ public class WeatherForecast {
             resultMap.put(temperature.getKey(), temperature.getValue() + 1.0); // [1]
         }
         return resultMap;
+    }
+
+    public Double avarageTemperature() {
+        Double avarageTemp= 0.0;
+
+        for (Map.Entry<String, Double> temperature :
+                temperatures.getTemperatures().entrySet()) {
+
+            avarageTemp = avarageTemp + temperature.getValue();
+
+        }
+        return avarageTemp/5;
+
+    }
+
+    public Double medianTemperature() {
+        Double medianTemp = 0.0;
+        List listOfTemp = new ArrayList();
+
+        for (Map.Entry<String, Double> temperature :
+                temperatures.getTemperatures().entrySet()) {
+
+            listOfTemp.add(temperature.getValue());
+        }
+        Collections.sort(listOfTemp);
+
+        medianTemp = (Double) listOfTemp.get(2);
+        return medianTemp;
     }
 }
