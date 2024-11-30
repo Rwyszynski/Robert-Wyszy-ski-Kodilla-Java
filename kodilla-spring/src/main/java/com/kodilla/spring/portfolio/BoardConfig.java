3a16a2d18d85d69d5510af47717ee3b90cc41fe7
Robert Wyszynski <robertointerwento@gmail.com>
2024-11-30 08:53:53 +0100

    13.3. Konfiguracja przy pomocy klasy Java

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,34 +1,28 @@
 package com.kodilla.spring.portfolio;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BoardConfig {
 
-    @Autowired
-    @Qualifier("toDoList")
-    TaskList taskList;
-
     @Bean
-    public Board board() {
-        return new Board();
+    public Board board(TaskList toDoList, TaskList inProgressList, TaskList doneList) {
+        return new Board(toDoList, inProgressList, doneList);
     }
 
-    @Bean(name = "toDoList")
+    @Bean
     public TaskList toDoList() {
         return new TaskList();
     }
 
-    @Bean(name = "inProgressList")
+    @Bean
     public TaskList inProgressList() {
         return new TaskList();
     }
 
-    @Bean(name = "completedList")
-    public TaskList completedList() {
+    @Bean
+    public TaskList doneList() {
         return new TaskList();
     }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,20 +1,49 @@
 package com.kodilla.spring.portfolio;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class BoardTestSuite {
 
+    @Autowired
+    private Board board;
+    @Autowired
+    private TaskList toDoList;
+    @Autowired
+    private TaskList inProgressList;
+    @Autowired
+    private TaskList doneList;
+
     @Test
-    void testTaskAdd(){}
-    //Given
-    ApplicationContext context = new AnnotationConfigApplicationContext(Board.class);
-    //When
-    Board board = context.getBean("board", Board.class);
+    void test1TaskAdd() {
+        //Given
+        //When
+        toDoList.addTask("task1");
 
-    //Then
+        //Then
+        Assertions.assertEquals("task1", board.getToDoList().getTasks().get(0));
+    }
 
+    @Test
+    void test2TaskAdd() {
+        //Given
+        //When
+        inProgressList.addTask("task2");
+
+        //Then
+        Assertions.assertEquals("task2", board.getInProgressList().getTasks().get(0));
+    }
+
+    @Test
+    void test3TaskAdd() {
+        //Given
+        //When
+        doneList.addTask("task3");
+
+        //Then
+        Assertions.assertEquals("task3", board.getDoneList().getTasks().get(0));
+    }
 }
