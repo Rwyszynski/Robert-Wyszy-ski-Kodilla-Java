de758389caaffcc538904ba3e6e37cb9df79c5e8
Robert Wyszynski <robertointerwento@gmail.com>
2024-12-04 15:24:32 +0100

    14.6. Debugger rodowiska IntelliJ

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testAddingItems.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.loop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class testAddingItems {
+
+    @Test
+    public void testAddingItems() {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing.loop;
+
+import org.junit.jupiter.api.Test;
+
+public class testLoopAndConditionalBreakpoints {
+
+    @Test
+    void testLoop()
+}
