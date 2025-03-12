cf94288be99c9bdaa08e921e46de19f3c67eb419
Robert Wyszynski <robertointerwento@gmail.com>
2025-03-12 14:27:49 +0100

    29.1. Selenium Webdriver

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -0,0 +1,22 @@
+package com.kodilla.testing2.ebay;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class EBayTestingApp {
+
+    public static final String SEARCHFIELD = "_nkw"; // "gh-ac"
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        driver.get("https://www.ebay.com");
+
+        driver.findElement(By.xpath("//*[@id=\"L2AGLb\"]/div")).click();
+
+        WebElement searchField = driver.findElement(By.name(SEARCHFIELD));      // id
+        searchField.sendKeys("Laptop");
+        searchField.submit();
+    }
+}
